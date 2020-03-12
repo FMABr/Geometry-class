1f784f0b9a014f7f6f2b06cc208d8fe45b110fd7
FMABr <Aluno@MAQ18LAB02>
2020-03-12 16:35:17 -0300

    Finalização do UsuarioService e pesquisa de usuarios por nome

--- geometryclass/src/main/java/com/ifam/geometryclass/data/dao/DaoGenerico.java
+++ geometryclass/src/main/java/com/ifam/geometryclass/data/dao/DaoGenerico.java
@@ -1,10 +1,14 @@
 package com.ifam.geometryclass.data.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DaoGenerico<Entidade, Chave> extends CrudRepository<Entidade, Chave>{
 
-	public Optional<Entidade> findByNome(String nome);	
+	public Optional<Entidade> findByNomeLike(String nome);
+	public List<Entidade> findByNomeContaining(String nome);
 }
--- geometryclass/src/main/java/com/ifam/geometryclass/service/UsuarioService.java
+++ geometryclass/src/main/java/com/ifam/geometryclass/service/UsuarioService.java
@@ -1,66 +1,105 @@
 package com.ifam.geometryclass.service;
 
 import com.ifam.geometryclass.data.dao.DaoGenerico;
+import com.ifam.geometryclass.data.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
-	
-  private DaoGenerico<Usuario, UUID> dao; 
 
-  public int createUsuario(Usuario usuario){
-    try{
-      dao.save(usuario);
-    } catch (IllegalArgumentException ex){
-      ex.printStackTrace();
-    }
-  }
+	private DaoGenerico<Usuario, UUID> dao;
 
-  public Optional<Usuario> readUsuarioById(UUID id){
-    try{
-      dao.findById(id);
-    } catch (IllegalArgumentException ex){
-      ex.printStackTrace();
-    }
-  }
+	public int createUsuario(Usuario usuario) {
+		try {
+			dao.save(usuario);
+			return 0;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+			return -1;
+		}
+	}
 
-  public Optional<Usuario> readUsuarioByNome(String nome){
-    try{
-      dao.findByNome(nome);
-    } catch (IllegalArgumentException ex){
-      ex.printStackTrace();
-    }
-  }
+	public Usuario readUsuarioById(UUID id) {
+		Optional<Usuario> leitura = Optional.empty();
 
-  public Usuario readAllUsuarios(){
-    try{
-      dao.findAll();
-    } catch (IllegalArgumentException ex){
-      ex.printStackTrace();
-    }
-  }
+		try {
+			leitura = dao.findById(id);
+			return leitura.isPresent() ? leitura.get() : null;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
 
-  public int updateUsuario(Usuario usuario){
-    try{
-      dao.save(usuario);
-    } catch (IllegalArgumentException ex){
-      ex.printStackTrace();
-    }
-  }
+	public Usuario readUsuarioByNome(String nome) {
+		Optional<Usuario> leitura = Optional.empty();
 
-  public int deleteUsuario(Usuario usuario){
-    try{
-      dao.delete(usuario);
-    } catch (IllegalArgumentException ex){
-      ex.printStackTrace();
-    }
-  };
+		try {
+			leitura = dao.findByNomeLike(nome);
+			return leitura.isPresent() ? leitura.get() : null;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
 
-  public int deleteUsuarioById(UUID id){
-    try{
-      dao.deleteById(id);
-    } catch (IllegalArgumentException ex){
-      ex.printStackTrace();
-    }
-  }
+	public List<Usuario> readAllUsuarios() {
+		List<Usuario> leitura = new ArrayList<>();
+
+		try {
+			dao.findAll().forEach(leitura::add);
+			return leitura;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+	public List<Usuario> readAllUsuarios(String nome) {
+		List<Usuario> leitura = new ArrayList<>();
+
+		try {
+			dao.findByNomeContaining(nome).forEach(leitura::add);
+			return leitura;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+	public int updateUsuario(Usuario usuario) {
+		try {
+			dao.save(usuario);
+			return 0;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+			return -1;
+		}
+	}
+
+	public int deleteUsuario(Usuario usuario) {
+		try {
+			dao.delete(usuario);
+			return 0;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+			return -1;
+		}
+	};
+
+	public int deleteUsuarioById(UUID id) {
+		try {
+			dao.deleteById(id);
+			return 0;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
+			return -1;
+		}
+	}
 }
