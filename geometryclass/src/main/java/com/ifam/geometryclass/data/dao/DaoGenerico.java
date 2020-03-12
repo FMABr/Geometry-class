4fe3e118763b112c166c421b999dea1d3ea7e248
FMABr <felipemuamorim@gmail.com>
2020-03-12 18:36:22 +0000

    Implementação do UsuarioService

--- geometryclass/src/main/java/com/ifam/geometryclass/data/dao/DaoGenerico.java
+++ geometryclass/src/main/java/com/ifam/geometryclass/data/dao/DaoGenerico.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DaoGenerico<Entidade, Chave> extends CrudRepository<Entidade, Chave>{
 
-	public Entidade findByNome(String nome);	
+	public Optional<Entidade> findByNome(String nome);	
 }
--- geometryclass/src/main/java/com/ifam/geometryclass/service/UsuarioService.java
+++ geometryclass/src/main/java/com/ifam/geometryclass/service/UsuarioService.java
@@ -1,9 +1,66 @@
 package com.ifam.geometryclass.service;
 
+import com.ifam.geometryclass.data.dao.DaoGenerico;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
 	
-	
+  private DaoGenerico<Usuario, UUID> dao; 
+
+  public int createUsuario(Usuario usuario){
+    try{
+      dao.save(usuario);
+    } catch (IllegalArgumentException ex){
+      ex.printStackTrace();
+    }
+  }
+
+  public Optional<Usuario> readUsuarioById(UUID id){
+    try{
+      dao.findById(id);
+    } catch (IllegalArgumentException ex){
+      ex.printStackTrace();
+    }
+  }
+
+  public Optional<Usuario> readUsuarioByNome(String nome){
+    try{
+      dao.findByNome(nome);
+    } catch (IllegalArgumentException ex){
+      ex.printStackTrace();
+    }
+  }
+
+  public Usuario readAllUsuarios(){
+    try{
+      dao.findAll();
+    } catch (IllegalArgumentException ex){
+      ex.printStackTrace();
+    }
+  }
+
+  public int updateUsuario(Usuario usuario){
+    try{
+      dao.save(usuario);
+    } catch (IllegalArgumentException ex){
+      ex.printStackTrace();
+    }
+  }
+
+  public int deleteUsuario(Usuario usuario){
+    try{
+      dao.delete(usuario);
+    } catch (IllegalArgumentException ex){
+      ex.printStackTrace();
+    }
+  };
+
+  public int deleteUsuarioById(UUID id){
+    try{
+      dao.deleteById(id);
+    } catch (IllegalArgumentException ex){
+      ex.printStackTrace();
+    }
+  }
 }
